69aba341dd8f1fbd9636c29396d10d86886e0b2c
tims13 <405362140@qq.com>
2021-05-09 16:17:10 +0800

    add string lower

--- utils.py
+++ utils.py
@@ -6,17 +6,14 @@ def sentence_token_nltk(str):
     return sent_tokenize_list
 
 def trim_string(x, first_n_words = 200):
-
+    x = x.lower()
     x = x.split(maxsplit=first_n_words)
     x = ' '.join(x[:first_n_words])
-
     return x
 
 def save_checkpoint(save_path, model, valid_loss):
-
     if save_path == None:
         return
-    
     state_dict = {'model_state_dict': model.state_dict(),
                   'valid_loss': valid_loss}
     
@@ -24,36 +21,25 @@ def save_checkpoint(save_path, model, valid_loss):
     print(f'Model saved to ==> {save_path}')
 
 def load_checkpoint(load_path, model, dev):
-    
     if load_path==None:
         return
-    
     state_dict = torch.load(load_path, map_location=dev)
     print(f'Model loaded from <== {load_path}')
-    
     model.load_state_dict(state_dict['model_state_dict'])
     return state_dict['valid_loss']
 
-
 def save_metrics(save_path, train_loss_list, valid_loss_list, global_steps_list):
-
     if save_path == None:
         return
-    
     state_dict = {'train_loss_list': train_loss_list,
                   'valid_loss_list': valid_loss_list,
                   'global_steps_list': global_steps_list}
-    
     torch.save(state_dict, save_path)
     print(f'Model saved to ==> {save_path}')
 
-
 def load_metrics(load_path, dev):
-
     if load_path==None:
         return
-    
     state_dict = torch.load(load_path, map_location=dev)
     print(f'Model loaded from <== {load_path}')
-    
     return state_dict['train_loss_list'], state_dict['valid_loss_list'], state_dict['global_steps_list']
